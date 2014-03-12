4f9ba8af0ee5660dd359bc5aeafee585576ce1c8
John Paul Ashenfelter <johnpaul@transitionpoint.com>
2014-03-12 12:02:32 -0400

    using parameter for container

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -10,19 +10,21 @@ class Bottles
   def verse(number)
     case number
     when 0
-      "No more #{container}s of #{contents} #{location}, no more #{container}s of #{contents}.\nGo to the store and buy some more, 99 #{container}s of #{contents} #{location}.\n"
+      "No more #{container} of #{contents} #{location}, no more #{container} of #{contents}.\nGo to the store and buy some more, 99 #{container} of #{contents} #{location}.\n"
     when 1
-      "#{number} #{container} of #{contents} #{location}, #{number} #{container} of #{contents}.\nTake it down and pass it around, no more #{container}s of #{contents} #{location}.\n"
+      "#{number} #{container(number)} of #{contents} #{location}, #{number} #{container(number)} of #{contents}.\nTake it down and pass it around, no more #{container} of #{contents} #{location}.\n"
+    when 2
+      "#{number} #{container} of #{contents} #{location}, #{number} #{container} of #{contents}.\nTake one down and pass it around, #{number-1} #{container(number-1)} of #{contents} #{location}.\n"
     else
-      "#{number} #{container}s of #{contents} #{location}, #{number} #{container}s of #{contents}.\nTake one down and pass it around, #{number-1} #{container}s of #{contents} #{location}.\n"
+      "#{number} #{container} of #{contents} #{location}, #{number} #{container} of #{contents}.\nTake one down and pass it around, #{number-1} #{container(number-1)} of #{contents} #{location}.\n"
     end
   end
 end
 
 private
 
-def container
-  "bottle"
+def container(count=99)
+  count > 1 ? "bottles" : "bottle"
 end
 
 def contents
