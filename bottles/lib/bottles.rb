f94d7ad4eae53272527124e1ea6e2b3301c59458
John Paul Ashenfelter <johnpaul@transitionpoint.com>
2014-03-12 12:16:26 -0400

    cleaned up line1

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -10,11 +10,11 @@ class Bottles
   def verse(number)
     case number
     when 0
-      "No more #{container} of #{contents} #{location}, say_count(number) #{container} of #{contents}.\nGo to the store and buy some more, 99 #{container} of #{contents} #{location}.\n"
+      "#{line1(number).capitalize}Go to the store and buy some more, 99 #{container} of #{contents} #{location}.\n"
     when 1
-      "#{say_count(number)} #{container(number)} of #{contents} #{location}, #{say_count(number)} #{container(number)} of #{contents}.\nTake it down and pass it around, no more #{container} of #{contents} #{location}.\n"
+      "#{line1(number).capitalize}Take it down and pass it around, no more #{container} of #{contents} #{location}.\n"
     else
-      "#{say_count(number)} #{container} of #{contents} #{location}, #{say_count(number)} #{container} of #{contents}.\nTake one down and pass it around, #{say_count(number-1)} #{container(number-1)} of #{contents} #{location}.\n"
+      "#{line1(number).capitalize}Take one down and pass it around, #{say_count(number-1)} #{container(number-1)} of #{contents} #{location}.\n"
     end
   end
 end
@@ -26,7 +26,7 @@ def say_count(count)
 end
 
 def container(count=99)
-  count > 1 ? "bottles" : "bottle"
+  count == 1 ? "bottle" : "bottles"
 end
 
 def contents
@@ -37,5 +37,6 @@ def location
   "on the wall"
 end
 
-def action
+def line1(number)
+"#{say_count(number)} #{container(number)} of #{contents} #{location}, #{say_count(number)} #{container(number)} of #{contents}.\n"
 end
