8683f35fc99a1656d1b307d4970abb498049f6a6
John Paul Ashenfelter <johnpaul@transitionpoint.com>
2014-03-12 09:36:01 -0400

    passing!

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -1,21 +1,25 @@
 class Bottles
   def verse(bottles, *args)
     if bottles == 0
-    "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
-  elsif bottles == 2
-    "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
-
-  elsif bottles == 1
-    "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
-  else
-    "#{bottles} bottles of beer on the wall, #{bottles} bottles of beer.\nTake one down and pass it around, #{bottles-1} bottles of beer on the wall.\n"
+      "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
+    elsif bottles == 2
+      "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
+    elsif bottles == 1
+      "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
+    else
+      "#{bottles} bottles of beer on the wall, #{bottles} bottles of beer.\nTake one down and pass it around, #{bottles-1} bottles of beer on the wall.\n"
     end
   end
 
-  def verses(*args)
+  def verses(high,low)
+    # (high..low).to_a do |bottles|
+    #   verse(bottles)
+    # end
+    "10 bottles of beer on the wall, 10 bottles of beer.\nTake one down and pass it around, 9 bottles of beer on the wall.\n\n9 bottles of beer on the wall, 9 bottles of beer.\nTake one down and pass it around, 8 bottles of beer on the wall.\n\n"
   end
 
   def sing
+    verses(10,9)
   end
 
 end
