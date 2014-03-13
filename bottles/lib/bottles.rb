1a7b32413af9307649c5dca48843a35a1fe21f1a
John Paul Ashenfelter <johnpaul@transitionpoint.com>
2014-03-13 10:22:10 -0400

    cleanup unused code

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -8,17 +8,6 @@ class Bottles
   end
 
   def verse(number)
-    case number
-    when 0
-      "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
-    when 1
-      "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
-    when 2
-      "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
-    else
-      "#{number} bottles of beer on the wall, #{number} bottles of beer.\nTake one down and pass it around, #{number-1} bottles of beer on the wall.\n"
-    end
-
     "#{inventory(number)} #{container(number)} #{liquid} #{location}, ".capitalize +
     "#{inventory(number)} #{container(number)} #{liquid}.\n" +
     "#{action(number)}, " +
